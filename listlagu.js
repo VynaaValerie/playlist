334aa87af8e6bcb9a3ac7a97bd552ed923a14fb6
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-08-12 22:52:09 +0700

    Add files via upload

---
+++ listlagu.js
@@ -0,0 +1,102 @@
+const songs = [
+    {
+        title: "Teramini - Ghea",
+        file: "/audio/Teramini-Ghea.mp3",
+        image: "https://telegra.ph/file/cdc3e5c8b924d57a36060.jpg"
+    },
+    {
+        title: "Sal Priadi - Gala Bunga",
+        file: "/audio/SalPriadi-Gala-bunga.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Bertaut- Nadin Amizah",
+        file: "/audio/Bertaut-Nadin-Amizah.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Ghea Indrawari - Jiwa Yang Bersedih",
+        file: "/audio/Ghea Indrawari-Jiwa-Yang-Bersedih.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Bohongi hati - mahalini",
+        file: "/audio/Bohongi-hati-mahalini.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "DUKA - last chilid",
+        file: "/audio/DUKA-last-chilid.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "INDAH - TAK SEMPURNA",
+        file: "/audio/INDAH-TAK-SEMPURNA.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Mendua - Astrid",
+        file: "/audio/Mendua-Astrid.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Sekuat Hatimu - LastChild",
+        file: "/audio/Sekuat-Hatimu-Last-Child.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Wizz Baker - Rindu Rumah",
+        file: "/audio/Wizz-Baker-Rindu-Rumah.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "Mahalini - bawadia kembali",
+        file: "/audio/Mahalini-bawadia-kembali.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    },
+    {
+        title: "AJARKAN AKU - ARVIAN DWI",
+        file: "/audio/AJARKAN-AKU-ARVIAN-DWI.mp3",
+        image: ""
+    },
+    {
+        title: "Takut - Idgitaf",
+        file: "/audio/Takut-Idgitaf.mp3",
+        image: ""
+    },
+    {
+        title: "ANDMESH - HANYA RINDU",
+        file: "/audio/ANDMESH-HANYA-RINDU.mp3",
+        image: ""
+    },
+    {
+        title: "TAK KAN HILANG - Budi Doremi",
+        file: "/audio/TAK-KAN-HILANG-Budi-Doremi.mp3",
+        image: ""
+    },
+    {
+        title: "TULUS - Nala",
+        file: "/audio/TULUS-Nala.mp3",
+        image: ""
+    },
+    {
+        title: "Dudy Oris - Aku Yang Jatuh Cinta",
+        file: "/audio/Dudy-Oris-Aku-Yang-Jatuh-Cinta.mp3",
+        image: ""
+    },
+    {
+      title: "Lyodra - tak Dianggap",
+        file: "/audio/Lyodra-tak-Dianggap.mp3",
+        image: ""
+    },
+    {  
+       title: "Ku Ingin Pisah - Nabila Taqiyyah",
+        file: "/audio/Ku-Ingin-Pisah-Nabila-Taqiyyah.mp3",
+        image: ""
+    },
+    {   
+     title: "Ziva - Cukup",
+        file: "/audio/Ziva-Magnolya-Cukup.mp3",
+        image: "https://telegra.ph/file/a609844fd6f3ab99ac4ef.jpg"
+    }
+];
